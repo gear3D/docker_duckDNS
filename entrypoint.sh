68b5e43e3d61b69aa1b63311e2e02f697b939eab
gear3D <calebwright84@gmail.com>
2025-09-27 14:34:38 +1000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
-# Run DuckDNS update once at startup
+# Fail early if variables not set
+: "${DUCKDNS_DOMAIN:?Need to set DUCKDNS_DOMAIN}"
+: "${DUCKDNS_TOKEN:?Need to set DUCKDNS_TOKEN}"
+
+# Run DuckDNS update once at container start
 echo "$(date): (startup) $(curl -s "https://www.duckdns.org/update?domains=$DUCKDNS_DOMAIN&token=$DUCKDNS_TOKEN&ip=")"
 
 # Start cron in foreground
